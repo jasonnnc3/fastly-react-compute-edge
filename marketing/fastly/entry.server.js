e9cdaa359995feba9c4d6a356057901b1260edaa
jason <23511462+jasonnnnnnnnnnnnn@users.noreply.github.com>
2021-12-03 06:17:56 -0800

    Changes committed:
    	modified:   marketing/fastly/entry.server.js

--- marketing/fastly/entry.server.js
+++ marketing/fastly/entry.server.js
@@ -16,9 +16,16 @@ async function handleRequest({ request }) {
 
   const url = new URL(request.url);
 
+  // hardcoding in index.html until i figure out backends
+  // https://developer.fastly.com/learning/compute/javascript/#composing-requests-and-responses
   // if (url.pathname === '/client.js') {
-  //   return new Request('https://my-vite-webapp.s3.us-west-2.amazonaws.com/client.js', {
+  //   const res = await fetch('https://my-vite-webapp.s3.us-west-2.amazonaws.com/client.js', {
   //     method: 'GET',
+  //     backend: 'viteapps3',
+  //   });
+  //
+  //   return new Response(res.text(), {
+  //     status: res.status,
   //     headers: new Headers({ 'Content-Type': 'application/javascript; charset=utf-8' }),
   //   });
   // }
