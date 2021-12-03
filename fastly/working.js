812739674dde850129121a4c56b08e94ef2bb6fb
jason <23511462+jasonnnnnnnnnnnnn@users.noreply.github.com>
2021-12-03 04:17:01 -0800

    Changes committed:
    	modified:   fastly/working.js
    	modified:   package-lock.json
    	modified:   package.json

--- fastly/working.js
+++ fastly/working.js
@@ -1,7 +1,9 @@
 import 'regenerator-runtime/runtime.js';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { App } from 'src/app';
+import { Routes } from 'react-router';
+import { Route, Link } from 'react-router-dom';
+import { StaticRouter } from 'react-router-dom/server';
 
 addEventListener('fetch', (event) => event.respondWith(handleRequest(event)));
 
@@ -14,21 +16,55 @@ async function handleRequest({ request }) {
 
   const url = new URL(request.url);
 
-  if (url.pathname === '/') {
-    return new Response('<div>test123</div>', {
+  if (url.pathname === '/client.entry.js') {
+    return new Response("console.log('test')", {
       status: 200,
-      headers: new Headers({ 'Content-Type': 'text/html; charset=utf-8' }),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
     });
   }
 
-  if (url.pathname === '/react') {
-    return new Response(ReactDOMServer.renderToString(<App testProp="a prop" />), {
-      status: 200,
-      headers: new Headers({ 'Content-Type': 'text/html; charset=utf-8' }),
-    });
-  }
-
-  return new Response('The page you requested could not be found', {
-    status: 404,
-  });
+  return new Response(
+    ReactDOMServer.renderToString(
+      <StaticRouter location={url}>
+        <html>
+          <head>
+            <title>Server Rendered App</title>
+          </head>
+          <body>
+            <Routes>
+              <Route
+                path="*"
+                element={
+                  <>
+                    <div>not found</div>
+                    <Link to="/">to home</Link>
+                  </>
+                }
+              />
+              <Route
+                path="/"
+                element={
+                  <>
+                    <div>Home</div>
+                    <Link to="/about">to about</Link>
+                  </>
+                }
+              />
+              <Route
+                path="/about"
+                element={
+                  <>
+                    <div>About</div>
+                    <Link to="/">to home</Link>
+                  </>
+                }
+              />
+            </Routes>
+            <script src="/client.entry.js" />
+          </body>
+        </html>
+      </StaticRouter>
+    ),
+    { status: 200, headers: new Headers({ 'Content-Type': 'text/html; charset=utf-8' }) }
+  );
 }
