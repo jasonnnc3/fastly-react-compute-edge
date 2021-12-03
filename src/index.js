7d8bc30b729dbec41d8144c6afd31d47bd7786f3
jason <23511462+jasonnnnnnnnnnnnn@users.noreply.github.com>
2021-12-02 22:37:22 -0800

    Changes committed:
    	new file:   .babelrc
    	modified:   fastly.toml
    	modified:   npm-shrinkwrap.json
    	modified:   package.json
    	modified:   src/index.js
    	deleted:    src/welcome-to-compute@edge.html
    	modified:   webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,35 @@
-//! Default Compute@Edge template program.
-import welcomePage from "./welcome-to-compute@edge.html";
+import "regenerator-runtime/runtime.js";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-// The entry point for your application.
-//
-// Use this fetch event listener to define your main request handling logic. It could be
-// used to route based on the request properties (such as method or path), send
-// the request to a backend, make completely new requests, and/or generate
-// synthetic responses.
+const reactString = ReactDOMServer.renderToString(<div>test</div>);
+const htmlString = "<div>working test</div>";
 
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
-async function handleRequest(event) {
-  // Get the client request.
-  let req = event.request;
-
-  // Filter requests that have unexpected methods.
-  if (!["HEAD", "GET"].includes(req.method)) {
+async function handleRequest({ request }) {
+  if (!["HEAD", "GET"].includes(request.method)) {
     return new Response("This method is not allowed", {
       status: 405,
     });
   }
 
-  let url = new URL(req.url);
+  const url = new URL(request.url);
 
-  // If request is to the `/` path...
-  if (url.pathname == "/") {
-    // Below are some common patterns for Compute@Edge services using JavaScript.
-    // Head to https://developer.fastly.com/learning/compute/javascript/ to discover more.
-
-    // Create a new request.
-    // let bereq = new Request("http://example.com");
-
-    // Add request headers.
-    // req.headers.set("X-Custom-Header", "Welcome to Compute@Edge!");
-    // req.headers.set(
-    //   "X-Another-Custom-Header",
-    //   "Recommended reading: https://developer.fastly.com/learning/compute"
-    // );
-
-    // Create a cache override.
-    // let cacheOverride = new CacheOverride("override", { ttl: 60 });
-
-    // Forward the request to a backend.
-    // let beresp = await fetch(req, {
-    //   backend: "backend_name",
-    //   cacheOverride,
-    // });
-
-    // Remove response headers.
-    // beresp.headers.delete("X-Another-Custom-Header");
-
-    // Log to a Fastly endpoint.
-    // const logger = fastly.getLogger("my_endpoint");
-    // logger.log("Hello from the edge!");
-
-    // Send a default synthetic response.
-    return new Response(welcomePage, {
+  if (url.pathname === "/") {
+    return new Response(htmlString, {
       status: 200,
       headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
     });
   }
 
-  // Catch all other requests and return a 404.
+  if (url.pathname === "/react") {
+    return new Response(reactString, {
+      status: 200,
+      headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
+    });
+  }
+
   return new Response("The page you requested could not be found", {
     status: 404,
   });
