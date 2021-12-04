be627d53fa3f221a316d0343513b0620d6bbec4e
jason <23511462+jasonnnnnnnnnnnnn@users.noreply.github.com>
2021-12-03 16:53:36 -0800

    Changes committed:
    	modified:   api/fastly.toml
    	modified:   web/fastly.toml
    	modified:   web/src/fastly/entry.server.tsx
    	modified:   web/src/fastly/index.html

--- web/src/fastly/entry.server.tsx
+++ web/src/fastly/entry.server.tsx
@@ -22,19 +22,18 @@ async function handleRequest({ request }) {
 
   const url = new URL(request.url);
 
-  // hardcoding in index.html until i figure out backends
-  // https://developer.fastly.com/learning/compute/javascript/#composing-requests-and-responses
-  // if (url.pathname === '/client.js') {
-  //   const res = await fetch('https://my-vite-webapp.s3.us-west-2.amazonaws.com/client.js', {
-  //     method: 'GET',
-  //     backend: 'viteapps3',
-  //   });
-  //
-  //   return new Response(res.text(), {
-  //     status: res.status,
-  //     headers: new Headers({ 'Content-Type': 'application/javascript; charset=utf-8' }),
-  //   });
-  // }
+  // hardcoding in index.html until i figure out backends, i feel like theres a better way to do this, but it works for now
+  if (url.pathname === '/client.js') {
+    const res = await fetch('https://my-vite-webapp.s3.us-west-2.amazonaws.com/client.js', {
+      method: 'GET',
+      backend: 'web_static_s3',
+    });
+
+    return new Response(await res.text(), {
+      status: res.status,
+      headers: new Headers({ 'Content-Type': 'application/javascript' }),
+    });
+  }
 
   const ssrHtml = ReactDOMServer.renderToString(
     <StaticRouter location={url}>
